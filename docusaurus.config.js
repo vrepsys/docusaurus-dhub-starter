fd2956542c913372a95248632d95e40d98042adc
Valdemaras Repšys <valdemaras@gmail.com>
2026-02-26 13:55:30 +0200

    Adjust tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Docs Starter',
-  tagline: 'A Docusaurus starter template for dhub',
+  tagline: 'A Docusaurus starter template for Dhub',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
